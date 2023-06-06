8c95315bc5330f5671e30c2a6ba28da3d3b454f0
fkrdnjs <rakwon1617@naver.com>
2023-06-06 18:04:23 +0900

    my_strlen 동작 확인

--- main.c
+++ main.c
@@ -2,9 +2,13 @@
 
 #include <stdio.h>
 
+#include "my_strlen.h"
+
 int main(void)
 {
-    
+    char* str = "Hello, World!";
+    int length = my_strlen(str);
+    printf("Length of the string: %u\n", length);
 
     return 0;
 }

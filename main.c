3401c2480ab67dda0035e5e466972cc9089250f4
fkrdnjs <rakwon1617@naver.com>
2023-06-06 18:10:16 +0900

    my_strcpy 작동 확인

--- main.c
+++ main.c
@@ -3,12 +3,19 @@
 #include <stdio.h>
 
 #include "my_strlen.h"
+#include "my_strcpy.h"
 
 int main(void)
 {
+    //my_strlen 작동 코드
     char* str = "Hello, World!";
     int length = my_strlen(str);
     printf("Length of the string: %u\n", length);
+
+    //my_strcpy 작동 코드
+    char dest[20];
+    my_strcpy(dest, str);
+    printf("Copied string: %s\n", dest);
 
     return 0;
 }
